1520db2f3550e52ccefcd4613afff251b2157173
Raul <raulfernan000@gmail.com>
2022-01-01 17:59:40 +0100

    form fixed and code refactored for clarity

--- index.php
+++ index.php
@@ -19,9 +19,50 @@
 
     <h2>Add a country you'd like to visit:</h2>
 
-    <?php
-    require __DIR__ . '/inc/CountryForm.php';
-    ?>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <div>
+            <label for="input-name">
+                Country: <input type="text" id="input-name" name="input-name">
+            </label>
+        </div>
+
+        <div>
+            <label for="input-length">length of your trip:</label>
+            <div>
+                <label for="input-length">
+                    <input type="radio" id="input-length" name="prefered-length" value="Up to a week"> Up to a week
+                </label>
+
+                <label for="input-length">
+                    <input type="radio" id="input-length" name="prefered-length" value="Up to two weeks"> Up to 2 weeks
+                </label>
+
+                <label for="input-length">
+                    <input type="radio" id="input-length" name="prefered-length" value="Up to a month"> Up to a month
+                </label>
+
+                <label for="input-length">
+                    <input type="radio" id="input-length" name="prefered-length" value="Longer than a month"> Longer than a month
+                </label>
+            </div>
+        </div>
+
+        <div>
+            <label for="input-season">
+            Season of your trip: <select id="input-season" name="prefered-season">
+                <option value="Winter">Winter</option>
+                <option value="Spring">Spring</option>
+                <option value="Summer">Summer</option>
+                <option value="Autumn">Autumn</option>
+                <option value="Any" selected>Any</option>
+            </select>
+            </label>
+        </div>
+
+        <div>
+            <input type="submit" value="Submit">
+        </div>
+    </form>
 
     <?php
     require __DIR__ . '/inc/CountryFormPost.php';
@@ -53,9 +94,7 @@
     <h2>Foreign Films:</h2>
 
     <?php
-    require __DIR__ . '/inc/Movie.php';
-
-    require __DIR__ . '/inc/MovieCardsGen.php';
+    require __DIR__ . '/inc/MovieList.php';
 
     $sacrifice = new Movie("Offret", "Sweden, United Kingdom, France", "1986", "Andrei Tarkovsky");
 
@@ -66,7 +105,6 @@
     $movies = array($sacrifice, $roma, $theSeventhSeal);
 
     createMovieCards($movies);
-
     ?>
 
 </body>
